71fa8db2babb0a4ca83474384d15e81f6488cdeb
BOB WANDATI <153294508+BOBWANDATI@users.noreply.github.com>
2025-10-26 12:30:01 +0300

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -36,7 +36,7 @@ const Login = ({ onLogin }) => {
           return;
         }
 
-        const response = await fetch('http://localhost:5000/api/auth/login', {
+        const response = await fetch('https://pharmacy-backend-qrb8.onrender.com/api/auth/login', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
